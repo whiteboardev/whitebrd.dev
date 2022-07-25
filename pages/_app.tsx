3544a095fe2579e7196aff70e177ec535900f438
Joao Paulo Oliveira <joaop.oliveira1@protonmail.com>
2022-07-25 14:54:16 -0300

    release(alpha): releasing the alpha version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
+import React from 'react'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <Component {...pageProps} />
+    )
 }
 
 export default MyApp
